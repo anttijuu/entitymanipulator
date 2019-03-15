02b443ceb3d4893b5bb071897f060f0ebafbebd5
Antti Juustila <antti.juustila@gmail.com>
2019-03-15 14:58:42 +0200

    Better memory management with unique ptrs. Moved object creation to a function to make the example clearer.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,8 @@
 #include "XMLMarshaller.h"
 #include "JSONMarshaller.h"
 
+Entity * createEntities();
+
 int main(int argc, const char * argv[])
 {
    // Create a following structure of objects...
@@ -29,59 +31,25 @@ Where street, billing and work addresses have details (street name, post number 
     */
    try {
       
-      EntityComposite customers("customers", "");
-      // First customer.
-      EntityComposite * customer = new EntityComposite("customer", "FooBar Ltd");
-      customers.add(customer);
-      customer->add(new EntityLeaf("id", "9897765"));
-      customer->add(new EntityLeaf("type", "Company"));
-      EntityComposite * address = new EntityComposite("address", "Billing");
-      customer->add(address);
-      address->add(new EntityLeaf("street", "Technovillage Post Office"));
-      address->add(new EntityLeaf("postnumber", "FI-90570"));
-      address->add(new EntityLeaf("city", "Oulu"));
-      address = new EntityComposite("address", "Street");
-      customer->add(address);
-      address->add(new EntityLeaf("street", "Technology Village Road 11"));
-      address->add(new EntityLeaf("entrance", "B 21"));
-      address->add(new EntityLeaf("postnumber", "FI-90570"));
-      address->add(new EntityLeaf("city", "Oulu"));
-      // Second customer.
-      customer = new EntityComposite("customer", "Antti Juustila");
-      customers.add(customer);
-      customer->add(new EntityLeaf("id", "123456"));
-      customer->add(new EntityLeaf("type", "Person"));
-      address = new EntityComposite("address", "Home");
-      customer->add(address);
-      address->add(new EntityLeaf("street", "Grand Vista Boulevard 125"));
-      address->add(new EntityLeaf("entrance", "A1"));
-      address->add(new EntityLeaf("postnumber", "JE3-10560"));
-      address->add(new EntityLeaf("city", "St Brelades"));
-      address = new EntityComposite("address", "Work");
-      customer->add(address);
-      address->add(new EntityLeaf("street", "Linnanmaa"));
-      address->add(new EntityLeaf("entrance", "R"));
-      address->add(new EntityLeaf("postnumber", "FI-90014"));
-      address->add(new EntityLeaf("city", "Oulun yliopisto"));
+      std::unique_ptr<Entity> customers(createEntities());
       
       // ...and give the marshaller to the root entity.
       std::cout << "Marshal objects to XML..." << std::endl << std::endl;
       // Create the marshaller..
       XMLMarshaller xmlMarshaller(std::cout);
-      customers.accept(xmlMarshaller);
+      customers->accept(xmlMarshaller);
       
       std::cout << std::endl << "Marshal objects to JSON..." << std::endl << std::endl;
       JSONMarshaller jsonMarshaller(std::cout);
-      customers.accept(jsonMarshaller);
+      customers->accept(jsonMarshaller);
       
       std::cout << "Cloning root entity and removing customer Antti Juustila from clone..." << std::endl << std::endl;
-      Entity * newComposite = customers.clone();
+      std::unique_ptr<Entity> newComposite(customers->clone());
       newComposite->remove({"customer", "Antti Juustila"});
       std::cout << "Marshal objects to XML..." << std::endl << std::endl;
       newComposite->accept(xmlMarshaller);
       std::cout << std::endl << "Marshal objects to JSON..." << std::endl << std::endl;
       newComposite->accept(jsonMarshaller);
-      delete newComposite;
       
    } catch (std::exception & e) {
       std::cout << "ERROR: " << e.what() << std::endl;
@@ -90,3 +58,40 @@ Where street, billing and work addresses have details (street name, post number 
    return 0;
 }
 
+Entity * createEntities() {
+   std::unique_ptr<EntityComposite> customers(new EntityComposite("customers", ""));
+   // First customer.
+   EntityComposite * customer = new EntityComposite("customer", "FooBar Ltd");
+   customers->add(customer);
+   customer->add(new EntityLeaf("id", "9897765"));
+   customer->add(new EntityLeaf("type", "Company"));
+   EntityComposite * address = new EntityComposite("address", "Billing");
+   customer->add(address);
+   address->add(new EntityLeaf("street", "Technovillage Post Office"));
+   address->add(new EntityLeaf("postnumber", "FI-90570"));
+   address->add(new EntityLeaf("city", "Oulu"));
+   address = new EntityComposite("address", "Street");
+   customer->add(address);
+   address->add(new EntityLeaf("street", "Technology Village Road 11"));
+   address->add(new EntityLeaf("entrance", "B 21"));
+   address->add(new EntityLeaf("postnumber", "FI-90570"));
+   address->add(new EntityLeaf("city", "Oulu"));
+   // Second customer.
+   customer = new EntityComposite("customer", "Antti Juustila");
+   customers->add(customer);
+   customer->add(new EntityLeaf("id", "123456"));
+   customer->add(new EntityLeaf("type", "Person"));
+   address = new EntityComposite("address", "Home");
+   customer->add(address);
+   address->add(new EntityLeaf("street", "Grand Vista Boulevard 125"));
+   address->add(new EntityLeaf("entrance", "A1"));
+   address->add(new EntityLeaf("postnumber", "JE3-10560"));
+   address->add(new EntityLeaf("city", "St Brelades"));
+   address = new EntityComposite("address", "Work");
+   customer->add(address);
+   address->add(new EntityLeaf("street", "Linnanmaa"));
+   address->add(new EntityLeaf("entrance", "R"));
+   address->add(new EntityLeaf("postnumber", "FI-90014"));
+   address->add(new EntityLeaf("city", "Oulun yliopisto"));
+   return customers.release();
+}
